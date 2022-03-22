aa3efa71a37140d0fe2d4029c975413fa762af59
Alessio Stalla <alessiostalla@gmail.com>
2022-03-22 14:36:25 +0000

    Attempt to clean up and unload cl+ssl before dumping the image

--- src/binary-sbcl.lisp
+++ src/binary-sbcl.lisp
@@ -1,3 +1,5 @@
+(when cl+ssl:*ssl-global-context*
+  (cl+ssl:ssl-ctx-free cl+ssl:*ssl-global-context*)
+  (setf cl+ssl:*ssl-global-context* nil))
 (map nil #'cffi:close-foreign-library (cffi:list-foreign-libraries))
-(print *features*)
 (save-lisp-and-die "portofino" :executable t :toplevel #'portofino-cli:main)
