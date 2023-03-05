c358144e71d9fe11c91083a607025dbd9d2eef5e
Alessio Stalla <alessiostalla@gmail.com>
2023-03-05 17:30:31 +0100

    Password prompt broken on win32, add conditional

--- src/password-prompt.lisp
+++ src/password-prompt.lisp
@@ -15,14 +15,14 @@
 (defun prompt-for-value (prompt)
   (capi:prompt-for-string prompt))
 
-#+sbcl
+#+(and sbcl (not win32))
 (defun echo-off ()
   (let ((tm (sb-posix:tcgetattr sb-sys:*tty*)))
     (setf (sb-posix:termios-lflag tm)
       (logandc2 (sb-posix:termios-lflag tm) sb-posix:echo))
     (sb-posix:tcsetattr sb-sys:*tty* sb-posix:tcsanow tm)))
 
-#+sbcl
+#+(and sbcl (not win32))
 (defun echo-on ()
   (let ((tm (sb-posix:tcgetattr sb-sys:*tty*)))
     (setf (sb-posix:termios-lflag tm)
@@ -33,10 +33,10 @@
 (defun prompt-for-passphrase (prompt)
   (format t "~&~a " prompt)
   (force-output)
-  #+sbcl (echo-off)
+  #+(and sbcl (not win32)) (echo-off)
   (unwind-protect
        (read-line)
-    #+sbcl (echo-on)))
+    #+(and sbcl (not win32)) (echo-on)))
 
 #+capi
 (defun prompt-for-passphrase (prompt)
